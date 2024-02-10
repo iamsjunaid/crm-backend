831cb6c919541e4705ecc8b822aeab7a4af41db0
Junaid Syed <sjunaid626@gmail.com>
2024-02-10 20:02:04 +0530

    Add contacts controller methods

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::ContactsController < ApplicationController
+  def index
+    render json: Contact.all
+  end
+
+  def show
+    render json: Contact.find(params[:id])
+  end
+
+  def create
+    contact = Contact.new(contact_params)
+    if contact.save
+      render json: contact, status: :created
+    else
+      render json: contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    contact = Contact.find(params[:id])
+    if contact.update(contact_params)
+      render json: contact
+    else
+      render json: contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    contact = Contact.find(params[:id])
+    contact.destroy
+  end
+
+  private 
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :customer_id)
+  end
 end
