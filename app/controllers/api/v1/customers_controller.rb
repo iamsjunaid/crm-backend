fcd1ece7e0ad240f36c157f7ec0a3efd68c2f374
Junaid Syed <sjunaid626@gmail.com>
2024-02-10 20:01:50 +0530

    Add customer controller methods

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::CustomersController < ApplicationController
+  def index
+    render json: Customer.all
+  end
+
+  def show
+    render json: Customer.find(params[:id])
+  end
+
+  def create
+    customer = Customer.new(customer_params)
+    if customer.save
+      render json: customer, status: :created
+    else
+      render json: customer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    customer = Customer.find(params[:id])
+    if customer.update(customer_params)
+      render json: customer
+    else
+      render json: customer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email)
+  end
 end
