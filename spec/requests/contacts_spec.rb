50849f4e0dc755f8d8afe1c903a42e03040b00e6
Junaid Syed <sjunaid626@gmail.com>
2024-02-11 10:17:36 +0530

    Test show endpoint: customers

--- spec/requests/contacts_spec.rb
+++ spec/requests/contacts_spec.rb
@@ -7,3 +7,12 @@ RSpec.describe 'Contacts', type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "GET #show" do
+    it "returns http success" do
+      customer = Customer.create(name: "John Doe", email: "john@mail.com")
+      contact = Contact.create(name: "Jane Doe", email: "jane@mail.com", customer: customer)
+      get "/api/v1/contacts/#{contact.id}"
+      expect(response).to have_http_status(:success)
+    end
+  end
